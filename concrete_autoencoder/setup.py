c0651f434dd8add673c8b4fcd7664eebed85120a
mattlevy15 <mattlevy15@gmail.com>
2021-12-09 13:13:24 -0600

    Update setup.py

--- concrete_autoencoder/setup.py
+++ concrete_autoencoder/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="concrete_autoencoder",
-    version="0.0.2",
+    name="concrete_autoencoder_new",
+    version="0.0.1",
     author="Muhammed Fatih Balin",
     author_email="m.f.balin@gmail.com",
     description="An implementation of Concrete Autoencoders",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mattlevy15/Concrete-Autoencoders",
+    url="https://github.com/mattlevy15/Concrete-Autoencoders/concrete_autoencoder_new",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
